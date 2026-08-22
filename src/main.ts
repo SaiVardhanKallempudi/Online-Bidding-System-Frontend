df003aafa67daa1608b17cc438d15d6ed53a708b
Sai Vardhan Kallempudi <153695133+SaiVardhanKallempudi@users.noreply.github.com>
2026-08-23 01:44:32 +0530

    Remove Vercel Web Analytics initialization
    Removed Vercel Web Analytics initialization from main.ts.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 (window as any).global = window;
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { App } from './app/app';
-import { inject } from '@vercel/analytics';
+import { App } from './app/app'
 
-// Initialize Vercel Web Analytics
-inject();
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
