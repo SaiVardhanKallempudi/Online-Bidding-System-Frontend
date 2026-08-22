99dda33b051453dbe866f311cd2e4da412e267e0
Sai Vardhan Kallempudi <153695133+SaiVardhanKallempudi@users.noreply.github.com>
2026-08-23 02:00:55 +0530

    Update production API and WebSocket URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://bidmart-backend-2m9h.onrender.com/api',
-  wsUrl: 'https://bidmart-backend-2m9h.onrender.com/ws-bidding',
-  googleOAuthUrl: 'https://bidmart-backend-2m9h.onrender.com/oauth2/authorization/google'
+  apiUrl: 'https://api.bidmart.me/api',
+  wsUrl: 'https://api.bidmart.me/ws-bidding',
+  googleOAuthUrl: 'https://api.bidmart.me/oauth2/authorization/google'
 };
